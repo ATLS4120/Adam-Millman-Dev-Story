55ea89a5923ba665c5058c4d1c16731a042b69fd
Adam Millman <amillman20@gmail.com>
2024-09-16 18:47:58 -0600

    did this work?

---
+++ dev-app/dev-app/dev_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dev_appApp.swift
+//  dev-app
+//
+//  Created by Adam Millman on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct dev_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
